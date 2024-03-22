f3f1c77703f9811c23249d62cc077619025acb9c
hianshul07 <hianshul07@gmail.com>
2024-03-22 23:56:36 +0530

    prop-types integration

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,15 @@
 import { Children } from 'react';
 
-const Button = ({ children }) => {
+const Button = ({
+	children,
+	primary,
+	secondary,
+	success,
+	warning,
+	danger,
+	outline,
+	rounded,
+}) => {
 	return <button>{children}</button>;
 };
 export default Button;
