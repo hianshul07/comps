09c6005dec217b1c0d7a5b8961b00d27fd290d3f
hianshul07 <hianshul07@gmail.com>
2024-03-22 23:37:28 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Button from "./components/Button"
+
+const App = () => {
+  return (
+    <div>
+      <div><Button>{'hehehe'}</Button></div>
+      <div><Button>{'car'}</Button></div>
+    </div>
+  )
+}
+export default App
---
+++ src/components/Button.jsx
@@ -0,0 +1,6 @@
+import { Children } from 'react';
+
+const Button = ({ children }) => {
+	return <button>{children}</button>;
+};
+export default Button;
