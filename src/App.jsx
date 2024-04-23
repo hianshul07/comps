1cf34e8afafbfd88d56d987f06c88b6988782c2c
hianshul07 <hianshul07@gmail.com>
2024-04-23 15:26:49 +0530

    accordion comp done

--- src/App.jsx
+++ src/App.jsx
@@ -10,18 +10,18 @@ const App = () => {
 		{
 			label: 'Can I use Javascript on a project?',
 			content:
-				'You can use React on any project you want. You can use React on any project you want. You can use React on any project you want. You can use React on any project you want.',
+				'You can use Javascript on any project you want. You can use Javascript on any project you want. You can use Javascript on any project you want. You can use Javascript on any project you want.',
 		},
 		{
 			label: 'Can I use CSS on a project?',
 			content:
-				'You can use React on any project you want. You can use React on any project you want. You can use React on any project you want. You can use React on any project you want.',
+				'You can use CSS on any project you want. You can use CSS on any project you want. You can use CSS on any project you want. You can use CSS on any project you want. ',
 		},
 	];
 
 	return (
 		<div>
-			<Accordion items={items}/>
+			<Accordion items={items} />
 		</div>
 	);
 };
--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -1,9 +1,36 @@
+import { useState } from 'react';
+import { FaChevronCircleDown, FaChevronCircleLeft } from 'react-icons/fa';
+
 const Accordion = ({ items }) => {
-	const renderedItems = items.map((item) => {
+	const [expandedIndex, setExpandedIndex] = useState(-1);
+
+	const handleClick = (nextIndex) => {
+		if (nextIndex === expandedIndex) {
+			setExpandedIndex(-1);
+		} else {
+			setExpandedIndex(nextIndex);
+		}
+	};
+
+	const renderedItems = items.map((item, index) => {
+		const isExpanded = index === expandedIndex;
+
+		const icon = (
+			<span className='text-2xl'>
+				{isExpanded ? <FaChevronCircleDown /> : <FaChevronCircleLeft />}
+			</span>
+		);
+
 		return (
-			<div key={item.label}>
-				<h1>{item.label}</h1>
-				<p>{item.content}</p>
+			<div key={item.label} className='cursor-pointer'>
+				<div
+					onClick={() => handleClick(index)}
+					className='flex items-center justify-between p-2 cursor-pointer border-b-2 bg-gray-50'
+				>
+					{item.label}
+					{icon}
+				</div>
+				{isExpanded && <div className=' border-b-2 p-4 bg-gray-200'>{item.content}</div>}
 			</div>
 		);
 	});
